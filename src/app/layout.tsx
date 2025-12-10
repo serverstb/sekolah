df005666378871d9c10977f96fb6854dc145e13f
Ryan Dewantoro <rnplur@gmail.com>
2025-12-10 05:20:55 +0000

    judul title Absensi Sekolah rubah menjadi Manajemen Sekolah

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: "Absensi Sekolah",
+  title: "Manajemen Sekolah",
   description: "Sistem absensi modern untuk sekolah.",
 };
 
